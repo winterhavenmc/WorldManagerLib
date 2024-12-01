2be7b195f4204a16d01702cdc7572b45beff1f93
Tim Savage <tim.savage@gmail.com>
2024-12-01 11:16:27 -0600

    remove unnecessary testing constructors

--- src/test/java/com/winterhavenmc/util/worldmanager/PluginMain.java
+++ src/test/java/com/winterhavenmc/util/worldmanager/PluginMain.java
@@ -1,29 +1,14 @@
 package com.winterhavenmc.util.worldmanager;
 
-import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.plugin.java.JavaPluginLoader;
 
-import java.io.File;
 
 /**
  * The main class for WorldManagerTest plugin
  */
-@SuppressWarnings("unused")
 public class PluginMain extends JavaPlugin {
 
 	public WorldManager worldManager;
-
-
-	public PluginMain() {
-		super();
-	}
-
-
-	protected PluginMain(JavaPluginLoader loader, PluginDescriptionFile descriptionFile, File dataFolder, File file) {
-		super(loader, descriptionFile, dataFolder, file);
-	}
-
 
 	@Override
 	public void onEnable() {
